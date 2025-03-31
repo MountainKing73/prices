7237a7f5ecf8a56ff9d562fcfe450786deaec148
James Cummings <jamescummings@gmail.com>
2025-03-31 12:49:29 -0400

    reverted to 32 bit numbers, but mean calc is 64 bit

--- src/main.rs
+++ src/main.rs
@@ -21,16 +21,16 @@ fn main() {
     }
 }
 
-fn convert_number(num: &[u8]) -> i64 {
+fn convert_number(num: &[u8]) -> i32 {
     let mut bin_string = "".to_string();
     for n in num {
         bin_string.push_str(&format!("{:08b}", n));
     }
 
-    u64::from_str_radix(&bin_string, 2).unwrap() as i64
+    u32::from_str_radix(&bin_string, 2).unwrap() as i32
 }
 
-fn convert_response(num: i64) -> [u8; 4] {
+fn convert_response(num: i32) -> [u8; 4] {
     let bin_string = format!("{:032b}", num);
 
     let mut result: [u8; 4] = [0; 4];
@@ -42,7 +42,7 @@ fn convert_response(num: i64) -> [u8; 4] {
 }
 
 fn process_request(stream: &mut TcpStream) {
-    let mut entries: HashMap<u64, i64> = HashMap::new();
+    let mut entries: HashMap<u32, i32> = HashMap::new();
     let mut reader = BufReader::new(stream.try_clone().expect("stream clone failed"));
 
     let mut buffer = [0; 9];
@@ -56,25 +56,26 @@ fn process_request(stream: &mut TcpStream) {
         println!("Received: {:?}", buffer);
 
         if buffer[0] == b'I' {
-            let timestamp = convert_number(&buffer[1..5]) as u64;
+            let timestamp = convert_number(&buffer[1..5]) as u32;
             let price = convert_number(&buffer[5..]);
             entries.insert(timestamp, price);
         } else if buffer[0] == b'Q' {
-            let timestamp1 = convert_number(&buffer[1..5]) as u64;
-            let timestamp2 = convert_number(&buffer[5..]) as u64;
+            let timestamp1 = convert_number(&buffer[1..5]) as u32;
+            let timestamp2 = convert_number(&buffer[5..]) as u32;
 
-            let mut total = 0;
+            let mut total: i64 = 0;
             let mut count = 0;
             for entry in &entries {
                 if timestamp1 <= *entry.0 && timestamp2 >= *entry.0 {
-                    total += entry.1;
+                    println!("found price: {}", entry.1);
+                    total += *entry.1 as i64;
                     count += 1;
                 }
             }
 
-            let mut result: i64 = 0;
+            let mut result: i32 = 0;
             if count > 0 {
-                result = total / count;
+                result = (total / count) as i32;
             }
 
             let _ = stream.write(&convert_response(result));
