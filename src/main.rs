a1d6a4948889610d499206de8fbe546b572494d3
James Cummings <jamescummings@gmail.com>
2025-03-31 11:28:53 -0400

    updated to use 64 bit numbers

--- src/main.rs
+++ src/main.rs
@@ -21,16 +21,16 @@ fn main() {
     }
 }
 
-fn convert_number(num: &[u8]) -> i32 {
+fn convert_number(num: &[u8]) -> i64 {
     let mut bin_string = "".to_string();
     for n in num {
         bin_string.push_str(&format!("{:08b}", n));
     }
 
-    u32::from_str_radix(&bin_string, 2).unwrap() as i32
+    u64::from_str_radix(&bin_string, 2).unwrap() as i64
 }
 
-fn convert_response(num: i32) -> [u8; 4] {
+fn convert_response(num: i64) -> [u8; 4] {
     let bin_string = format!("{:032b}", num);
 
     let mut result: [u8; 4] = [0; 4];
@@ -42,7 +42,7 @@ fn convert_response(num: i32) -> [u8; 4] {
 }
 
 fn process_request(stream: &mut TcpStream) {
-    let mut entries: HashMap<u32, i32> = HashMap::new();
+    let mut entries: HashMap<u64, i64> = HashMap::new();
     let mut reader = BufReader::new(stream.try_clone().expect("stream clone failed"));
 
     let mut buffer = [0; 9];
@@ -53,13 +53,15 @@ fn process_request(stream: &mut TcpStream) {
             break;
         }
 
+        println!("Received: {:?}", buffer);
+
         if buffer[0] == b'I' {
-            let timestamp = convert_number(&buffer[1..5]) as u32;
+            let timestamp = convert_number(&buffer[1..5]) as u64;
             let price = convert_number(&buffer[5..]);
             entries.insert(timestamp, price);
         } else if buffer[0] == b'Q' {
-            let timestamp1 = convert_number(&buffer[1..5]) as u32;
-            let timestamp2 = convert_number(&buffer[5..]) as u32;
+            let timestamp1 = convert_number(&buffer[1..5]) as u64;
+            let timestamp2 = convert_number(&buffer[5..]) as u64;
 
             let mut total = 0;
             let mut count = 0;
@@ -70,7 +72,7 @@ fn process_request(stream: &mut TcpStream) {
                 }
             }
 
-            let mut result: i32 = 0;
+            let mut result: i64 = 0;
             if count > 0 {
                 result = total / count;
             }
